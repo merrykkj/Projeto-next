e57eb4c86212dfc474d5e93f3077437d0ad39918
VITORIA PEREIRA DE LUCENA <24251125@educ123.sp.senai.br>
2025-04-04 10:43:16 -0300

    Auto update:

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,9 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Carrossel from "../components/carrossel/carrossel.jsx";
 
 export default function Home() {
   return (
-    <>
-      <div className="background">
-        <img src="./home/fundo1.jpg" alt="Imagem de fundo" />
-      </div>
-      <div className="img">
-        <img src="./home/macarrao.png" alt="img2"></img>
-      </div>
-    </>
+    <main>
+      <Carrossel />
+    </main>
   );
 }
